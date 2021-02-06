3829c3f89952322a93708dd331bf9c4ad34a9e19
ANISH BHOWMICK <52849781+anish833@users.noreply.github.com>
2021-02-06 09:58:27 -0500

    Add files via upload

---
+++ my.py
@@ -0,0 +1,15 @@
+def challenge_three( xord ): #bruteforcing to find the key
+	
+	result = []
+	for i in range(97,123):
+		result = pwnlib.util.fiddling.xor(xored, chr(i))  #not fully automated but see the byte that make more sense
+		print(result)
+		
+def with_key( xord ): #decrypting with the key
+	
+	result = pwnlib.util.fiddling.xor(xored, 120)
+	return result
+
+xored = unhexlify("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
+get = with_key(xored)
+print("\nChallenge 3 ---> " + get.replace(b'\x00', b' ').replace(b'\x07', b'\'').swapcase().decode("ascii")) #Bad things happens sometimes to python3...converting back to ascii
